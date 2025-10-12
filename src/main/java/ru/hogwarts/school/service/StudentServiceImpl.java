5fe8aa6c3647c8d013a1775ee3cde68608924889
Zinakhan93 <Zinakhan93@mail.ru>
2025-10-13 00:08:25 +0300

    Потоки

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -76,5 +77,28 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
+    // потоки
+    @Override
+    public List<Student> getFirstSixStudents() {
+        logger.info("Was invoked method for getting first six students");
+        return studentRepository.findAll().stream()
+                .limit(6)
+                .collect(Collectors.toList());
+    }
 
+    @Override
+    public void printStudentName(Student student) {
+        System.out.println("Student: " + student.getName() + " (Thread: " + Thread.currentThread().getName() + ")");
+    }
+
+    @Override
+    public synchronized void printStudentNameSynchronized(Student student) {
+        try {
+            // Имитация длительной операции
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Student: " + student.getName() + " (Thread: " + Thread.currentThread().getName() + ")");
+    }
 }

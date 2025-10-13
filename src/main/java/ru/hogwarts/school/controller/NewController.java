fac4b86217739358b60eeb6db8437b0ef2da1993
Zinakhan93 <Zinakhan93@mail.ru>
2025-10-14 00:36:05 +0300

    Потоки добаление сервиса для логики

--- src/main/java/ru/hogwarts/school/controller/NewController.java
+++ src/main/java/ru/hogwarts/school/controller/NewController.java
@@ -1,31 +1,27 @@
 package ru.hogwarts.school.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.service.CalculationService;
 
 import java.util.stream.IntStream;
 
 @RestController
 
 public class NewController {
+    private final CalculationService calculationService;
+
+    public NewController(CalculationService calculationService) {
+        this.calculationService = calculationService;
+    }
+
     @GetMapping("/sum")
     public int calculateSum() {
-        long startTime1 = System.currentTimeMillis();
-        int sum = IntStream.iterate(1, a -> a + 1)
-                .limit(1_000_000)
-                .reduce(0, (a, b) -> a + b );
-        long endTime1 = System.currentTimeMillis();
-        System.out.println("Calculation time: " + (endTime1 - startTime1) + " ms");
-        return sum;
+        return calculationService.calculateSum();
     }
 
     @GetMapping("/sum-fast")
     public int calculateSumFast() {
-        long startTime2 = System.currentTimeMillis();
-        int sumFast = IntStream.rangeClosed(1, 1_000_000)
-                .parallel()
-                .sum();
-        long endTime2 = System.currentTimeMillis();
-        System.out.println("Calculation time2: " + (endTime2 - startTime2) + " ms");
-        return sumFast;
+        return calculationService.calculateSumFast();
     }
 }
